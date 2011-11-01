a1c5354f4c5d4175ece122076bea0d60cc0538e5
Matthias Bethke <matthias@towiski.de>
2011-11-01 02:59:16 -0600

    New model classes; completed Mailbox controller; started Alias controller

---
+++ lib/Ashafix/Model/Quota.pm
@@ -0,0 +1,39 @@
+package Ashafix::Model::Quota;
+#===============================================================================
+#
+#         FILE:  Quota.pm
+#
+#  DESCRIPTION:  Old quota table
+#
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Matthias Bethke (mbethke), matthias@towiski.de
+#      COMPANY:  Zonarix S.A.
+#      VERSION:  1.0
+#      CREATED:  10/29/2011 03:22:20 PM
+#     REVISION:  ---
+#===============================================================================
+use 5.010;
+use strict;
+use warnings;
+use base 'Ashafix::Model::Base';
+
+our %snippets = (
+    find_by_user_old    => "SELECT * FROM %table_quota WHERE username=?",
+    delete_old          => "DELETE FROM %table_quota WHERE username=?",
+    find_by_user_new    => "SELECT * FROM %table_quota2 WHERE username=?",
+    delete_new          => "DELETE FROM %table_quota2 WHERE username=?",
+);
+
+sub new {
+    my ($class, $config) = @_;
+    my $self = $class->SUPER::new($config);
+
+    foreach(grep { /_old$/ } keys %queries) {
+        no strict 'refs';
+        my $nosuff  = substr($_, 0, -4); # cut off suffix
+        my $aliasto = $nosuff . ($config->{newquota} ? '_new' : '_old' );
+        *{"$nosuff"} = *{"$aliasto"};    # alias find_by_user => find_by_user_{old,new}
+    }
+    return $self;
+}
---
+++ lib/Ashafix/Model/Vacation.pm
@@ -0,0 +1,27 @@
+package Ashafix::Model::Vacation;
+#===============================================================================
+#
+#         FILE:  Vacation.pm
+#
+#  DESCRIPTION:  Vacation model, uses vacation and vacation_notification tables
+#
+#         BUGS:  ---
+#        NOTES:  ---
+#       AUTHOR:  Matthias Bethke (mbethke), matthias@towiski.de
+#      COMPANY:  Zonarix S.A.
+#      VERSION:  1.0
+#      CREATED:  10/29/2011 03:23:39 PM
+#     REVISION:  ---
+#===============================================================================
+use 5.010;
+use strict;
+use warnings;
+use base 'Ashafix::Model::Base';
+
+our %queries = (
+    check_by_mbox   => 'SELECT 1 FROM %table_vacation WHERE email=? AND domain=?',
+    delete_vacation => 'DELETE FROM %table_vacation WHERE email=? AND domain=?',
+);
+
+1;
+
