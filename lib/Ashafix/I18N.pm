7be268ed353b68793e9ef595a158a886ed0e8599
Matthias Bethke <matthias@towiski.de>
2012-07-11 11:53:45 -0600

    Fix error while querying languages

--- lib/Ashafix/I18N.pm
+++ lib/Ashafix/I18N.pm
@@ -3,57 +3,17 @@ package Ashafix::I18N;
 #
 #         FILE:  I18N.pm
 #
-#  DESCRIPTION:  
+#  DESCRIPTION:  Internationalization base class for Ashafix
 #
-#        FILES:  ---
-#         BUGS:  ---
-#        NOTES:  ---
 #       AUTHOR:  Matthias Bethke (mbethke), matthias@towiski.de
 #      COMPANY:  Zonarix S.A.
 #      VERSION:  1.0
 #      CREATED:  09/09/2011 03:58:15 PM
-#     REVISION:  ---
 #===============================================================================
 
 use strict;
 use warnings;
 use utf8;
-
-#sub supported_languages {
-#    return {
-##       'code'  => 'native language name - language name in english'
-#        'bg'    => 'Български - Bulgarian',
-#        'ca'    => 'Català - Catalan',
-#        'cn'    => '中文 - Chinese simplified (gb2312)',
-#        'tw'    => '中文 - Chinese traditional',
-#        'cs'    => 'Česky - Czech',
-#        'da'    => 'Dansk - Danish',
-#        'de'    => 'Deutsch - German',
-#        'en'    => 'English',
-#        'es'    => 'Español - Spanish',
-#        'et'    => 'Eesti - Estonian',
-#        'eu'    => 'Euskara - Basque',
-#        'fi'    => 'Suomi - Finnish',
-#        'fo'    => 'Faroese',
-#        'fr'    => 'Français - French',
-#        'hr'    => 'Hrvatski - Croatian',
-#        'hu'    => 'Magyar - Hungarian',
-#        'is'    => 'Icelandic',
-#        'it'    => 'Italiano - Italian',
-#        'ja'    => '日本語 - Japanese',
-#        'lt'    => 'Lietuvių - Lithuanian',
-#        'mk'    => 'Macedonian - Macedonian',
-#        'nl'    => 'Nederlands - Dutch',
-#        'nb'    => 'Norsk (bokmål) - Norwegian (bokmål)',
-#        'nn'    => 'Norsk (nynorsk) - Norwegian (nynorsk)',
-#        'pl'    => 'Polski - Polish',
-#        'pt-br' => 'Português - Brazilian portuguese',
-#        'ru'    => 'Русский - Russian',
-#        'sk'    => 'Slovenčina - Slovak',
-#        'sl'    => 'Slovenščina - Slovenian',
-#        'sv'    => 'Svenska - Swedish',
-#        'tr'    => 'Türkçe - Turkish',
-#    };
-#}
+use parent 'Locale::Maketext';
 
 1;
