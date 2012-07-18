34134710cb1231ab83ff7ee5ed2bf6e950f87a64
Matthias Bethke <matthias@towiski.de>
2012-07-18 16:22:11 -0600

    Updated password changing

--- lib/Ashafix/Controller/Misc.pm
+++ lib/Ashafix/Controller/Misc.pm
@@ -1,5 +1,6 @@
 package Ashafix::Controller::Misc;
 use Mojo::Base 'Ashafix::Controller';
+use Try::Tiny;
 
 sub send_mail {
     my $self = shift;
@@ -8,28 +9,31 @@ sub send_mail {
 
 sub chpassword {
     my $self = shift;
-    my $user = $self->session('user')->{name};
-    $self->req->method =~ /^(?:GET|HEAD)/ and return $self->render(username => $user);
+    my $username = $self->session('user')->name;
+    $self->req->method =~ /^(?:GET|HEAD)/ and return $self->render(username => $username);
 
     # POSTing new values
     my ($pw_old, $pw_new, $pw_new2) = map { $self->param($_) } qw / currentpw newpw newpw2 /;
-    my $uinfo = $self->verify_account($user, $pw_old) or return $self->render(
-        username              => $user,
+    my $user = $self->verify_account($username, $pw_old) or return $self->render(
+        username              => $username,
         password_current_text => $self->l('pPassword_password_current_text_error'),
     );
 
-    !length($pw_new) || $pw_new ne $pw_new2 and return $self->render(
-        username          => $user,
-        password_new_text => $self->l('pPassword_password_text_error'),
-    );
-
-    my $model_name = $self->session('user')->{roles}{admin} ? 'admin' : 'mailbox';
-    if($self->model($model_name)->update_password($self->app->pacrypt($pw_new), $user)->rows) {
-        $self->show_info_l('pPassword_result_success');
-    } else {
-        $self->show_error_l('pPassword_result_error');
+    if(!length($pw_new) || $pw_new ne $pw_new2) {
+        return $self->render(
+            username          => $username,
+            password_new_text => $self->l('pPassword_password_text_error'),
+        );
     }
-    return $self->render(username => $user);
+
+    $user->password($self->app->pacrypt($pw_new));
+    try {
+        $self->update_user($user);
+        $self->show_info_l('pPassword_result_success');
+    } catch {
+        $self->show_error_l('pPassword_result_error');
+    };
+    return $self->render(username => $username);
 }
 
 sub view_log {
