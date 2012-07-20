9d64be234d8d36be30caf7bd4b021a9bb17e4ab2
Matthias Bethke <matthias@towiski.de>
2012-07-20 15:05:02 -0600

    Added two methods for Local::AliasStatus

--- lib/Ashafix/Model/Alias.pm
+++ lib/Ashafix/Model/Alias.pm
@@ -17,4 +17,19 @@ package Ashafix::Model::Alias;
 
 use Mojo::Base 'Ashafix::Model::Base'; 
 
+# Return a list of destination addresses for a given alias
+sub list_gotos {
+    my ($self, $alias) = @_;
+    return $self->schema('alias')->get_goto_by_address($alias)->flat;
+}
+
+# Return the subset of the 2 or 3 address arguments that exists as aliases
+sub get_address {
+    my $self = shift;
+    my $s = $self->schema('alias');
+    2 == @_ and return $s->get_address_2(@_)->flat;
+    3 == @_ and return $s->get_address_3(@_)->flat;
+    die "Ashafix::Model::Alias::get_address needs 2 or 3 arguments";
+}
+
 1;
